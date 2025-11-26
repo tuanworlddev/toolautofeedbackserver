f09e06f679b5d1386d5aa8f4200a36f9d652ca00
tuanworddev <nanhtuan.it@gmail.com>
2025-11-26 16:05:30 +0700

    update port

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ connectDB();
 
 const app = require('./app');
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3400;
 app.listen(PORT, () => {
     console.log(`Server running at http://localhost:${PORT}`);
 })
